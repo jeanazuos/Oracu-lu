a0dcce92cfe47ebab1dc1edc859f3643217e7b61
Jean Santos <jsantos@igspo.ogmaster.local>
2020-05-18 15:30:07 -0300

    removed mongo connection info from pipelines

--- luizaScraper/luizaScraper/settings.py
+++ luizaScraper/luizaScraper/settings.py
@@ -67,10 +67,6 @@ ROBOTSTXT_OBEY = True
 
 ITEM_PIPELINES = {'luizaScraper.pipelines.MongoDBPipeline':300 }
 
-MONGODB_SERVER = "127.0.0.1"
-MONGODB_PORT = 27017
-MONGODB_DB = "luiza-deal"
-MONGODB_COLLECTION = "deal-of-day"
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
